56ee27c333a05fa9d9325bf73edd2a528d79d355
LorenzoDOrtona <dortonalorenzo@gmail.com>
2025-12-03 13:25:56 +0100

    json start

--- cmd/server/Requests.go
+++ cmd/server/Requests.go
@@ -5,20 +5,20 @@ import(
 	"log"
 )
 type req_base struct{
-	Type string
+	Type string `json:"type"`
 }
 type req_joinGame struct{
-	Token string
-	User_Type string //real or bot
-	Req_Type string
+	Token string `json:"token"`
+	User_Type string `json:"user_type"`//real or bot
+	Req_Type string `json:"req_type"`
 	//--	
-	Old_Game bool
-	ID_Game UUID.uuid	
+	Old_Game bool  `json:"type"`
+	ID_Game UUID.uuid `json:"type"`
 	//--
-	Game_Mode string //no cards, yes cards
-	Side_Measure int //9 if 9x9 (3x 3x3)
-	Wanted_Enemy UUID.uuid //or None
-
+	Game_Mode string `json:"type"` //no cards, yes cards
+	
+	Side_Measure int `json:"type"`  //9 if 9x9 (3x 3x3)
+	Wanted_Enemy UUID.uuid `json:"type"` //or None
 
 }
 type req_move struct{
